8ba6154f131bbe0fd13d0571944446c0cd6b4618
Shubham More <shubham.more@joshsoftware.com>
2023-02-07 16:23:36 +0530

    Modified routes file

--- src/Route.tsx
+++ src/Route.tsx
@@ -1,30 +1,16 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Navigate,
-  Route,
-  Routes,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import App from "./App";
 
 import { AuthProvider } from "./context/AuthProvider";
-
-import { ROUTES, PUBLIC_ROUTES } from "./constants";
-import App from "./App";
 
 const RouteComponent = () => {
   return (
     <Router>
       <AuthProvider>
         <Routes>
-          {/* {PUBLIC_ROUTES.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<route.component />}
-            />
-          ))}
-          <Route path="*" element={<Navigate to={ROUTES.HOME} replace />} /> */}
-          <Route path="/*" element={<App/>}/>
+          <Route path="/*" element={<App />} />
         </Routes>
       </AuthProvider>
     </Router>
